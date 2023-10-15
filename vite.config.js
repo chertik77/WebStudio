2142037dc937725a409a9793fb2c2e1d0a7a7d09
chertik77 <babychdenya@gmail.com>
2023-10-15 14:48:29 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({ plugins: [FullReload(['./**/**.html'])] });
+export default defineConfig({ plugins: [ViteMinifyPlugin({}), FullReload(['./**/**.html'])] });

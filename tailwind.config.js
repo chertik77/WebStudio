33ec5c803e7c1aa890179b79c336285fa7b0a611
chertik77 <babychdenya@gmail.com>
2023-10-15 20:27:36 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+export default {
+  content: ['./src/**/*.{html}'],
+  theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      raleway: ['Raleway', 'sans-serif'],
+    },
+    fontWeight: {
+      normal: '400',
+      medium: '500',
+      bold: '700',
+      extrabold: '800',
+    },
+    transitionTimingFunction: {
+      linear: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    },
+    transitionDuration: {
+      250: '250ms',
+    },
+    colors: {
+      primaryText: '#434455',
+      secondaryText: '#2e2f42',
+      thirdText: '#8e8f99',
+      lightText: ' #e7e9fc',
+      primary: '#4d5ae5',
+      secondary: '#404bbf',
+      success: '#31d0aa',
+      modalBg: '#fcfcfc',
+      modalMain: '#f4f4fd',
+    },
+  },
+};

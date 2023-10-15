5e1c6625b9da434db998dba87bf0c44427127ba3
chertik77 <babychdenya@gmail.com>
2023-10-15 18:23:57 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({ plugins: [ViteMinifyPlugin({}), FullReload(['./**/**.html'])] });
+export default defineConfig({
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+    },
+    outDir: '../dist',
+  },
+  plugins: [injectHTML(), ViteMinifyPlugin({}), FullReload(['./**/**.html'])],
+});

f455cc202118e0195c71ae56bf133208ed2433de
chertik77 <babychdenya@gmail.com>
2023-10-19 17:43:26 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,79 @@ export default {
       roboto: ['Roboto', 'sans-serif'],
       raleway: ['Raleway', 'sans-serif'],
     },
-    fontWeight: {
-      normal: '400',
-      medium: '500',
-      bold: '700',
-      extrabold: '800',
+    fontSize: {
+      sm: [
+        '12px',
+        {
+          lineHeight: '14px',
+          letterSpacing: '0.48px',
+          fontWeight: '400',
+        },
+      ],
+      '2sm': [
+        '12px',
+        {
+          lineHeight: '24px',
+          letterSpacing: '0.48px',
+          fontWeight: '400',
+        },
+      ],
+      md: [
+        '16px',
+        {
+          lineHeight: '24px',
+          letterSpacing: '0.32px',
+          fontWeight: '400',
+        },
+      ],
+      '2md': [
+        '16px',
+        {
+          lineHeight: '24px',
+          letterSpacing: '0.32px',
+          fontWeight: '500',
+        },
+      ],
+      '3md': [
+        '16px',
+        {
+          lineHeight: '24px',
+          letterSpacing: '0.64px',
+          fontWeight: '500',
+        },
+      ],
+      lg: [
+        '18px',
+        {
+          lineHeight: '21px',
+          letterSpacing: '0.54px',
+          fontWeight: '800',
+        },
+      ],
+      '2lg': [
+        '20px',
+        {
+          lineHeight: '24px',
+          letterSpacing: '0.4px',
+          fontWeight: '500',
+        },
+      ],
+      xl: [
+        '36px',
+        {
+          lineHeight: '40px',
+          letterSpacing: '0.72px',
+          fontWeight: '700',
+        },
+      ],
+      '2xl': [
+        '56px',
+        {
+          lineHeight: '60px',
+          letterSpacing: '1.12px',
+          fontWeight: '700',
+        },
+      ],
     },
     transitionTimingFunction: {
       linear: 'cubic-bezier(0.4, 0, 0.2, 1)',
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   root: 'src',
+  css: {},
   build: {
     rollupOptions: {
       input: glob.sync('./src/*.html'),
     },
     outDir: '../dist',
+    assetsDir: '',
+    cssCodeSplit: false,
   },
-  plugins: [injectHTML(), ViteMinifyPlugin()],
+  plugins: [injectHTML(), ViteMinifyPlugin(), tailwindcss()],
 });
